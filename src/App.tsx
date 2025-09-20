b422c17581a52e397b5a331aaaca36993311c96b
hilmyha <hilmyahmadhaidar@gmail.com>
2025-09-20 18:29:29 +0700

    ganti warna, background

--- src/App.tsx
+++ src/App.tsx
@@ -35,12 +35,9 @@ function useMasterMapping(sheetId: string, apiKey: string) {
 function App() {
   // Generate warna random
   function getRandomColor() {
-    const letters = "0123456789ABCDEF";
-    let color = "#";
-    for (let i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
+    const colors = ["#ef4444", "#22c55e", "#3b82f6", "#eab308"];
+    // red-500, green-500, blue-500, yellow-500
+    return colors[Math.floor(Math.random() * colors.length)];
   }
 
   const SHEET_ID = "1-H58WXJQHkCdxpjkyBn2tyT25jTiMJ3bb3wM2JViN7s";
@@ -154,7 +151,7 @@ function App() {
       return (
         existing || {
           option: name,
-          style: { backgroundColor: getRandomColor(), textColor: "#ffffff"},
+          style: { backgroundColor: getRandomColor(), textColor: "#ffffff" },
         }
       );
     });
@@ -163,133 +160,141 @@ function App() {
   }, [newName]);
 
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center gap-6 p-4 bg-gray-50">
-      {/* <h1 className="text-2xl font-bold">Spin Wheel Grup A-B-C</h1> */}
-
-      {data.length > 0 ? (
-        <>
-          <Wheel
-            mustStartSpinning={mustSpin}
-            prizeNumber={prizeNumber}
-            outerBorderWidth={0}
-            radiusLineWidth={0}
-            innerRadius={20}
-            fontSize={38}
-            data={data}
-            spinDuration={0.5}
-            onStopSpinning={handleStop}
-          />
-        </>
-      ) : (
-        <p className="text-red-500 font-semibold">
-          Belum ada data. Silakan tambah nama dulu!
-        </p>
-      )}
-
-      {/* Textarea sinkron */}
-      <div className="absolute bg-gray-100 right-0 top-0 h-full p-4 flex flex-col justify-center items-start gap-2">
-        <div className="flex flex-col gap-2 w-64">
-          <textarea
-            value={newName}
-            onChange={(e) => setNewName(e.target.value)}
-            onKeyDown={(e) => {
-              if (e.key === "Enter" && !e.shiftKey) {
-                e.preventDefault();
-                setNewName((prev) => prev + "\n");
-              }
-            }}
-            className="px-3 py-2 rounded bg-white h-[40vh] resize-none"
-            placeholder="Masukkan nama (satu per baris)"
-          />
-          <button
-            onClick={handleSpinClick}
-            className="px-4 py-2 bg-blue-500 text-white rounded disabled:bg-gray-400 w-full"
-            disabled={mustSpin}
-          >
-            SPIN
-          </button>
-        </div>
-      </div>
-
-      {/* Tabel hasil */}
-      <div className="w-full max-w-3xl mt-8">
-        <h2 className="text-xl font-semibold mb-4 text-gray-700">
-          Hasil Pembagian Grup
-        </h2>
-        <div className="overflow-x-auto rounded-lg shadow-md">
-          <table className="w-full border-collapse bg-white text-gray-700">
-            <thead>
-              <tr className="bg-gradient-to-r from-blue-500 to-indigo-500 text-white">
-                <th className="px-4 py-3 text-sm font-semibold">Grup A</th>
-                <th className="px-4 py-3 text-sm font-semibold">Grup B</th>
-                <th className="px-4 py-3 text-sm font-semibold">Grup C</th>
-              </tr>
-            </thead>
-            <tbody>
-              {Array.from(
-                {
-                  length: Math.max(
-                    groups.A.length,
-                    groups.B.length,
-                    groups.C.length
-                  ),
-                },
-                (_, i) => (
-                  <tr
-                    key={i}
-                    className={i % 2 === 0 ? "bg-gray-50" : "bg-white"}
-                  >
-                    <td className="px-4 py-2 capitalize border border-gray-200 text-center">
-                      {groups.A[i] || "-"}
-                    </td>
-                    <td className="px-4 py-2 capitalize border border-gray-200 text-center">
-                      {groups.B[i] || "-"}
-                    </td>
-                    <td className="px-4 py-2 capitalize border border-gray-200 text-center">
-                      {groups.C[i] || "-"}
-                    </td>
-                  </tr>
-                )
-              )}
-            </tbody>
-          </table>
-        </div>
-      </div>
-
-      {/* Popup pengumuman */}
-      {announcement && (
-        <div className="fixed w-[1200px] h-[500px] top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col items-center justify-center bg-gray-200 p-6 rounded shadow-lg z-50">
-          <h2 className="text-6xl font-bold mb-2 capitalize">
-            {announcement.name}
-          </h2>
-          <p className="text-3xl">
-            {announcement.message ? (
-              <span className="text-red-600">{announcement.message}</span>
-            ) : (
-              <>
-                Terpilih ke grup{" "}
-                <span className="font-semibold">{announcement.group}</span>
-              </>
-            )}
+    <div className="min-h-screen relative">
+      <div className="min-h-screen flex flex-col items-center justify-center gap-6 p-4 relative">
+        {/* <h1 className="text-8xl text-clip bg-gradient-to-l from-gray-400 to-white font-bold">VIPBET99</h1> */}
+        <div
+          className="absolute inset-0 bg-cover bg-center -z-10"
+          style={{
+            backgroundImage:
+              "url('https://images.unsplash.com/photo-1604871000636-074fa5117945?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
+          }}
+        />
+        <div className="absolute inset-0 bg-black/70 -z-10" />{" "}
+        {/* overlay gelap */}
+        {data.length > 0 ? (
+          <>
+            <Wheel
+              mustStartSpinning={mustSpin}
+              prizeNumber={prizeNumber}
+              outerBorderWidth={0}
+              radiusLineWidth={0}
+              innerRadius={20}
+              fontSize={38}
+              data={data}
+              spinDuration={0.5}
+              onStopSpinning={handleStop}
+            />
+          </>
+        ) : (
+          <p className="text-red-500 text-xl">
+            Belum ada data. Silakan tambah nama dulu!
           </p>
-
-          <div className="mt-8 flex items-center">
+        )}
+        {/* Textarea sinkron */}
+        <div className="absolute bg-gray-800/70 right-0 top-0 h-full p-4 flex flex-col justify-center items-start gap-2">
+          <div className="flex flex-col gap-2 w-64">
+            <textarea
+              value={newName}
+              onChange={(e) => setNewName(e.target.value)}
+              onKeyDown={(e) => {
+                if (e.key === "Enter" && !e.shiftKey) {
+                  e.preventDefault();
+                  setNewName((prev) => prev + "\n");
+                }
+              }}
+              spellCheck={false}
+              className="mb-4 px-3 py-2 rounded bg-gray-600 text-white h-[40vh] resize-none"
+              placeholder="Masukkan nama (satu per baris)"
+            />
             <button
-              onClick={handleExit}
-              className="mt-4 px-4 py-2 bg-blue-500 text-white rounded"
+              onClick={handleSpinClick}
+              className="px-4 py-2 text-lg font-semibold bg-gradient-to-r from-red-500 to-orange-500 text-white rounded disabled:bg-gray-400 w-full"
+              disabled={mustSpin}
             >
-              OK
-            </button>
-
-            <button
-              onClick={() => setAnnouncement(null)}
-              className="mt-4 ml-2 px-4 py-2 bg-gray-500 text-white rounded"
-            >
-              Tutup
+              Spin
             </button>
           </div>
         </div>
-      )}
+        {/* Tabel hasil */}
+        <div className="w-full max-w-3xl mt-8">
+          <h2 className="text-xl font-semibold mb-4 text-gray-100">
+            Hasil Pembagian Grup
+          </h2>
+          <div className="overflow-x-auto rounded-lg shadow-md">
+            <table className="w-full border-collapse bg-white text-gray-700">
+              <thead>
+                <tr className="bg-gradient-to-r from-red-500 to-orange-500 text-white">
+                  <th className="px-4 py-3 text-lg font-semibold">Grup A</th>
+                  <th className="px-4 py-3 text-lg font-semibold">Grup B</th>
+                  <th className="px-4 py-3 text-lg font-semibold">Grup C</th>
+                </tr>
+              </thead>
+              <tbody>
+                {Array.from(
+                  {
+                    length: Math.max(
+                      groups.A.length,
+                      groups.B.length,
+                      groups.C.length
+                    ),
+                  },
+                  (_, i) => (
+                    <tr
+                      key={i}
+                      className={i % 2 === 0 ? "bg-gray-600" : "bg-gray-600"}
+                    >
+                      <td className="px-4 py-2 capitalize text-white text-center">
+                        {groups.A[i] || "-"}
+                      </td>
+                      <td className="px-4 py-2 capitalize text-white text-center">
+                        {groups.B[i] || "-"}
+                      </td>
+                      <td className="px-4 py-2 capitalize text-white text-center">
+                        {groups.C[i] || "-"}
+                      </td>
+                    </tr>
+                  )
+                )}
+              </tbody>
+            </table>
+          </div>
+        </div>
+        {/* Popup pengumuman */}
+        {announcement && (
+          <div className="fixed w-[1200px] h-[500px] top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col items-center justify-center bg-gray-200 p-6 rounded shadow-lg z-50">
+            <h2 className="text-6xl font-bold mb-2 capitalize">
+              {announcement.name}
+            </h2>
+            <p className="text-3xl">
+              {announcement.message ? (
+                <span className="text-red-600">{announcement.message}</span>
+              ) : (
+                <>
+                  Terpilih ke grup{" "}
+                  <span className="font-semibold">{announcement.group}</span>
+                </>
+              )}
+            </p>
+
+            <div className="mt-8 flex items-center">
+              <button
+                onClick={handleExit}
+                className="mt-4 px-4 py-2 bg-red-500 text-white rounded"
+              >
+                OK
+              </button>
+
+              <button
+                onClick={() => setAnnouncement(null)}
+                className="mt-4 ml-2 px-4 py-2 bg-gray-500 text-white rounded"
+              >
+                Tutup
+              </button>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
